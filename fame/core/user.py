2cf42933cac7e3524edcf91b06a3d12ead605010
Augustin-FL <Augustin-FL@users.noreply.github.com>
2026-09-12 15:25:29 +0200

    Keep avatar generation locally

--- fame/core/user.py
+++ fame/core/user.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from base64 import b64encode
+from robohash import Robohash
 
 from fame.core.store import store
 from fame.common.utils import delete_from_disk
@@ -8,7 +9,7 @@ from fame.common.constants import AVATARS_ROOT
 from fame.common.mongo_dict import MongoDict
 
 
-class FilteredCollection():
+class FilteredCollection:
     def __init__(self, collection, filters):
         self.collection = collection
         self.filters = filters
@@ -28,12 +29,13 @@ class FilteredCollection():
         combined_filters.update(self.filters)
         return self.collection.count_documents(combined_filters)
 
+
 class User(MongoDict):
-    collection_name = 'users'
+    collection_name = "users"
 
     def __init__(self, values):
-        self['permissions'] = []
-        self['api_key'] = User.generate_api_key()
+        self["permissions"] = []
+        self["api_key"] = User.generate_api_key()
         MongoDict.__init__(self, values)
 
         self.is_authenticated = True
@@ -45,29 +47,29 @@ class User(MongoDict):
         self.analyses = FilteredCollection(store.analysis, self.filters())
 
     def get_id(self):
-        return self['auth_token']
+        return self["auth_token"]
 
     def filters(self):
-        if '*' in self['groups']:
+        if "*" in self["groups"]:
             return {}
         else:
-            return {'groups': {'$in': self['groups']}}
+            return {"groups": {"$in": self["groups"]}}
 
     def has_permission(self, permission):
-        return (permission in self['permissions']) or ('*' in self['permissions'])
+        return (permission in self["permissions"]) or ("*" in self["permissions"])
 
     def generate_avatar(self):
         s = b64encode(os.urandom(64))
         try:
-            response = requests.get("https://robohash.org/{}.png".format(s))
-            response.raise_for_status()
-            with open(os.path.join(AVATARS_ROOT, "{}.png".format(self['_id'])), 'wb') as f:
-                f.write(response.content)
+            rh = Robohash(s.decode())
+            rh.assemble()
+            with open(os.path.join(AVATARS_ROOT, f"{self['_id']}.png"), "wb") as f:
+                rh.img.save(f, format="png")
         except:
-            print(("Could not generate avatar for {}".format(self['email'])))
+            print(("Could not generate avatar for {}".format(self["email"])))
 
     def delete(self):
-        delete_from_disk(os.path.join(AVATARS_ROOT, "{}.png".format(self['_id'])))
+        delete_from_disk(os.path.join(AVATARS_ROOT, "{}.png".format(self["_id"])))
         super().delete()
 
     @staticmethod
